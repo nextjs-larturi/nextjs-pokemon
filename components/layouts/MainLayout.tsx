850deaa7b3cc7b106f0c871b35577ad673d05ccc
Leandro Arturi <lea.arturi@gmail.com>
2022-06-04 17:36:31 -0300

    Prod

--- components/layouts/MainLayout.tsx
+++ components/layouts/MainLayout.tsx
@@ -7,7 +7,12 @@ interface Props {
     title?: string;
 }
 
+const origin = (typeof window == 'undefined') ? '' :  window.location.origin;
+
 export const MainLayout: FC<Props> = ({ children, title }) => {
+
+  console.log(origin);
+
   return (
     <>
         <Head>
@@ -15,6 +20,9 @@ export const MainLayout: FC<Props> = ({ children, title }) => {
             <meta name='author' content='Leandro Arturi' />
             <meta name='description' content={`Información sobre el Pokemon ${title}`} />
             <meta name='keywords' content={`${title}, pokemon, pokedex`} />
+            <meta property="og:title" content={`Información sobre ${ title }`} />
+            <meta property="og:description" content={`Página de ${ title }`} />
+            <meta property="og:image" content={`${ origin }/img/banner.png`} />
         </Head>
 
         <Navbar />
